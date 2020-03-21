70bf537fff170140a97e1d4446ec81207ae60127
Lavisha Mascarenhas <lavishamascarenhas@gmail.com>
2020-03-21 19:24:49 +0000

    login code for LMS

---
+++ src/main/java/com/erp/gic/lms/login/LoginPageObject.java
@@ -0,0 +1,54 @@
+package com.erp.gic.lms.login;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPageObject {
+
+    public static WebDriver driver;
+
+    public LoginPageObject()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath="//*[@id=\"login-popup-2\"]/div/div[1]/a")
+    WebElement loginBtn;
+
+    @FindBy(name="user_login")
+    WebElement usernameTxtFld;
+
+    @FindBy(name="user_password")
+    WebElement passwordTxtFld;
+
+    @FindBy(name="wp-submit")
+    WebElement login;
+
+    public void launch(String url)
+    {
+        driver.get(url);
+    }
+
+    public void loginClick()
+    {
+        loginBtn.click();
+    }
+
+    public void getLoginDetails(String uname,String pwd)
+    {
+        usernameTxtFld.sendKeys(uname);
+        passwordTxtFld.sendKeys(pwd);
+        login.click();
+    }
+
+    public void browserQuit()
+    {
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/erp/gic/lms/login/feature/login.feature
@@ -0,0 +1,10 @@
+Feature: As a user validate the login
+
+  Scenario: Login functionality
+
+    Given I am on the application url
+    When I click on the login button
+    And I enter the username as "testing" and password as "4vzdIu9XMUW)ubtU*V#EYjo9" and click on login button
+    Then I should be successfully logged in and be on home page
+
+
